e495bbee37be538026d29cc0b2ef248c5b4cf1d2
Mr. Senko <atodorov@mrsenko.com>
2019-04-23 23:45:03 +0300

    Make pylint happy

--- tcms_github_marketplace/utils.py
+++ tcms_github_marketplace/utils.py
@@ -1,10 +1,32 @@
 # Copyright (c) 2019 Alexander Todorov <atodorov@MrSenko.com>
 
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
-
+import hmac
+import hashlib
 from datetime import datetime
 
-from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.http import HttpResponseForbidden, HttpResponseRedirect
+
+
+def verify_signature(request):
+    """
+        Verifies request comes from GitHub, see:
+        https://developer.github.com/webhooks/securing/
+    """
+    signature = request.META.get('HTTP_X_HUB_SIGNATURE', None)
+    if not signature:
+        return HttpResponseForbidden()
+
+    expected = 'sha1=' + hmac.new(  # pylint: disable=no-member
+        settings.KIWI_GITHUB_MARKETPLACE_SECRET,
+        msg=request.POST,
+        digestmod=hashlib.sha1).hex()
+
+    if not hmac.compare_digest(signature, expected):
+        return HttpResponseForbidden()
+
+    return True  # b/c of inconsistent-return-statements
 
 
 def handle_purchased(payload):
@@ -12,6 +34,11 @@ def handle_purchased(payload):
         Handle 'purchased' event, see:
         https://developer.github.com/marketplace/integrating-with-the-github-marketplace-api/github-marketplace-webhook-events/
     """
+    # format is 2017-10-25T00:00:00+00:00
+    effective_date = datetime.strptime(payload['effective_date'][:19],
+                                       '%Y-%m-%dT%H:%M:%S')
+    sender = payload['sender']['login']
+    purchase = payload['marketplace_purchase']
     plan_price = purchase['plan']['monthly_price_in_cents']
 
     # Free Marketplace plans have nothing to install so they
@@ -21,13 +48,6 @@ def handle_purchased(payload):
     # For now we don't even record this event in the database
     if plan_price == 0:
         return HttpResponseRedirect('/')
-
-
-    # format is 2017-10-25T00:00:00+00:00
-    effective_date = datetime.strptime(payload['effective_date'][:19],
-                                       '%Y-%m-%dT%H:%M:%S')
-    sender = payload['sender']['login']
-    purchase = payload['marketplace_purchase']
 
     raise NotImplementedError('Paid plans are not supported yet')
 
--- tcms_github_marketplace/views.py
+++ tcms_github_marketplace/views.py
@@ -3,11 +3,7 @@
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
 
 import json
-import hmac
-import hashlib
 
-from django.conf import settings
-from django.http import HttpResponseForbidden
 from django.views.generic.base import TemplateView
 
 from tcms_github_marketplace import utils
@@ -18,27 +14,11 @@ class PurchaseHook(TemplateView):
         Handles `marketplace_purchase` web hook as described at:
         https://developer.github.com/marketplace/listing-on-github-marketplace/configuring-the-github-marketplace-webhook/
     """
-    def verify_signature(self, request):
-        """
-            Verifies request comes from GitHub, see:
-            https://developer.github.com/webhooks/securing/
-        """
-        signature = request.META.get('HTTP_X_HUB_SIGNATURE', None)
-        if not signature:
-            return HttpResponseForbidden()
-
-        expected = 'sha1=' + hmac.new(settings.KIWI_GITHUB_MARKETPLACE_SECRET,
-                                      msg=request.POST,
-                                      digestmod=hashlib.sha1).hex()
-
-        if not hmac.compare_digest(signature, expected):
-            return HttpResponseForbidden()
-
     def post(self, request, *args, **kwargs):
         """
             Hook must be configured to receive JSON payload!
         """
-        self.verify_signature(request)
+        utils.verify_signature(request)
 
         payload = json.loads(request.POST)
 
@@ -47,4 +27,6 @@ class PurchaseHook(TemplateView):
         elif payload['action'] == 'cancelled':
             utils.handle_cancelled(payload)
 
-        raise NotImplementedError('Unsupported GitHub Marketplace hook action %s' % payload['action'])
+        raise NotImplementedError(
+            'Unsupported GitHub Marketplace hook action %s' %
+            payload['action'])
