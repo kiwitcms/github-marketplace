f05a5a27488f92aa83d3684b51ae093c12d5be25
Mr. Senko <atodorov@mrsenko.com>
2019-04-27 12:34:20 +0300

    Bump to version 0.2.1 b/c packaging

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def get_install_requires(path):
 
 setup(
     name='kiwitcms-github-marketplace',
-    version='0.2.0',
+    version='0.2.1',
     description='GitHub Marketplace integration for Kiwi TCMS',
     long_description=get_long_description(),
     author='Kiwi TCMS',
