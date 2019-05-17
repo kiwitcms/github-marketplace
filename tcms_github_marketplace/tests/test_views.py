e3218a6e1560444286ab1a97883af14abf5aa97d
Mr. Senko <atodorov@mrsenko.com>
2019-05-17 22:04:56 +0300

    Add more tests
    - Install URL redirects to Create Tenant page for paid plans
    - Create Tenant page redirects to:
      - / if no purchase
      - own tenant if tenant already exists
    - Create Tenant displays overriden template with data from purchase
    - Recurring billing

--- tcms_github_marketplace/tests/test_views.py
+++ tcms_github_marketplace/tests/test_views.py
@@ -5,13 +5,16 @@
 import json
 from http import HTTPStatus
 from unittest.mock import patch
+from datetime import datetime, timedelta
 
 from django.urls import reverse
 from django.conf import settings
+from django.test import RequestFactory
 from django.http import HttpResponseForbidden
 from django.contrib.auth import get_user_model
 
 from social_django.models import UserSocialAuth
+from tcms_tenants import utils as tcms_tenants_utils
 
 from tcms_github_marketplace import utils
 from tcms_github_marketplace.models import Purchase
@@ -191,6 +194,85 @@ class PurchaseHookTestCase(LoggedInTestCase):
         # initial ping responds with a pong
         self.assertContains(response, 'pong')
 
+    def test_recurring_billing_hook(self):
+        """
+            According to GitHub recurring billing events do not redirect to
+            Install URL but only send webhook payloads so we must
+            extend tenant.paid_until while handling the hook event
+        """
+        # tenant has expired yesterday (artificial example)
+        # b/c we will update only tenant which are currently/have been previously
+        # been paid for !!!
+        self.tenant.paid_until = datetime.now() - timedelta(days=1)
+        self.tenant.save()
+
+        payload = """
+{
+   "action":"purchased",
+   "effective_date":"2017-10-25T00:00:00+00:00",
+   "sender":{
+      "login":"%s",
+      "id":3877742,
+      "avatar_url":"https://avatars2.githubusercontent.com/u/3877742?v=4",
+      "gravatar_id":"",
+      "url":"https://api.github.com/users/username",
+      "html_url":"https://github.com/username",
+      "followers_url":"https://api.github.com/users/username/followers",
+      "following_url":"https://api.github.com/users/username/following{/other_user}",
+      "gists_url":"https://api.github.com/users/username/gists{/gist_id}",
+      "starred_url":"https://api.github.com/users/username/starred{/owner}{/repo}",
+      "subscriptions_url":"https://api.github.com/users/username/subscriptions",
+      "organizations_url":"https://api.github.com/users/username/orgs",
+      "repos_url":"https://api.github.com/users/username/repos",
+      "events_url":"https://api.github.com/users/username/events{/privacy}",
+      "received_events_url":"https://api.github.com/users/username/received_events",
+      "type":"User",
+      "site_admin":true,
+      "email":"username@email.com"
+   },
+   "marketplace_purchase":{
+      "account":{
+         "type":"Organization",
+         "id":18404719,
+         "login":"%s",
+         "organization_billing_email":"username@email.com"
+      },
+      "billing_cycle":"monthly",
+      "unit_count":1,
+      "on_free_trial":false,
+      "free_trial_ends_on":null,
+      "next_billing_date":null,
+      "plan":{
+         "id":435,
+         "name":"Public Tenant",
+         "description":"Basic Plan",
+         "monthly_price_in_cents":3200,
+         "yearly_price_in_cents":32000,
+         "price_model":"flat",
+         "has_free_trial":true,
+         "unit_name":"seat",
+         "bullets":[
+            "Is Basic",
+            "Because Basic "
+         ]
+      }
+   }
+}
+""".strip() % (self.tenant.owner.username, self.tenant.owner.username)
+        signature = utils.calculate_signature(settings.KIWI_GITHUB_MARKETPLACE_SECRET,
+                                              json.dumps(json.loads(payload)).encode())
+
+        # send marketplace_purchase hook
+        response = self.client.post(self.url,
+                                    json.loads(payload),
+                                    content_type='application/json',
+                                    HTTP_X_HUB_SIGNATURE=signature)
+        self.assertContains(response, 'ok')
+
+        # paid_until date was increased minimum 30 days
+        self.tenant.refresh_from_db()
+        self.assertGreater(self.tenant.paid_until, datetime.now()+timedelta(days=30))
+
 
 class InstallTestCase(LoggedInTestCase):
     @classmethod
@@ -283,6 +365,7 @@ class OtherInstallTestCase(LoggedInTestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.install_url = reverse('github_marketplace_install')
+        cls.purchase_hook_url = reverse('github_marketplace_purchase_hook')
 
     def test_visit_without_purchase(self):
         """
@@ -296,6 +379,76 @@ class OtherInstallTestCase(LoggedInTestCase):
 
         # redirect to / on public tenant
         self.assertRedirects(response, '/')
+
+    def test_purchased_paid_plan(self):
+        payload = """
+{
+   "action":"purchased",
+   "effective_date":"2017-10-25T00:00:00+00:00",
+   "sender":{
+      "login":"%s",
+      "id":3877742,
+      "avatar_url":"https://avatars2.githubusercontent.com/u/3877742?v=4",
+      "gravatar_id":"",
+      "url":"https://api.github.com/users/username",
+      "html_url":"https://github.com/username",
+      "followers_url":"https://api.github.com/users/username/followers",
+      "following_url":"https://api.github.com/users/username/following{/other_user}",
+      "gists_url":"https://api.github.com/users/username/gists{/gist_id}",
+      "starred_url":"https://api.github.com/users/username/starred{/owner}{/repo}",
+      "subscriptions_url":"https://api.github.com/users/username/subscriptions",
+      "organizations_url":"https://api.github.com/users/username/orgs",
+      "repos_url":"https://api.github.com/users/username/repos",
+      "events_url":"https://api.github.com/users/username/events{/privacy}",
+      "received_events_url":"https://api.github.com/users/username/received_events",
+      "type":"User",
+      "site_admin":true,
+      "email":"username@email.com"
+   },
+   "marketplace_purchase":{
+      "account":{
+         "type":"Organization",
+         "id":18404719,
+         "login":"%s",
+         "organization_billing_email":"username@email.com"
+      },
+      "billing_cycle":"monthly",
+      "unit_count":1,
+      "on_free_trial":false,
+      "free_trial_ends_on":null,
+      "next_billing_date":"2017-11-05T00:00:00+00:00",
+      "plan":{
+         "id":435,
+         "name":"Public Tenant",
+         "description":"Basic Plan",
+         "monthly_price_in_cents":3200,
+         "yearly_price_in_cents":32000,
+         "price_model":"flat",
+         "has_free_trial":true,
+         "unit_name":"seat",
+         "bullets":[
+            "Is Basic",
+            "Because Basic "
+         ]
+      }
+   }
+}
+""".strip() % (self.tester.username, self.tester.username)
+        signature = utils.calculate_signature(settings.KIWI_GITHUB_MARKETPLACE_SECRET,
+                                              json.dumps(json.loads(payload)).encode())
+
+        # first simulate marketplace_purchase hook
+        response = self.client.post(self.purchase_hook_url,
+                                    json.loads(payload),
+                                    content_type='application/json',
+                                    HTTP_X_HUB_SIGNATURE=signature)
+        self.assertContains(response, 'ok')
+
+        # then simulate redirection to Installation URL
+        response = self.client.get(self.install_url)
+
+        # purchases for paid plans redirect to Create Tenant page
+        self.assertRedirects(response, reverse('github_marketplace_create_tenant'))
 
 
 class CancelPlanTestCase(InstallTestCase):
@@ -391,3 +544,276 @@ class CancelPlanTestCase(InstallTestCase):
 
         # verify user is not present anymore
         self.assertFalse(get_user_model().objects.filter(username=self.tester.username).exists())
+
+
+class CreateTenantTestCase(LoggedInTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.create_tenant_url = reverse('github_marketplace_create_tenant')
+        cls.purchase_hook_url = reverse('github_marketplace_purchase_hook')
+
+    def test_visit_without_purchase(self):
+        """
+            If user visits the Create Tenant page
+            without having purchased plans from Marketplace first.
+        """
+        response = self.client.get(self.create_tenant_url)
+
+        # redirect to / on public tenant
+        self.assertRedirects(response, '/')
+
+    def test_visit_after_creation(self):
+        """
+            If user visits the Create Tenant page
+            when they already have a tenant created
+        """
+        payload = """
+{
+   "action":"purchased",
+   "effective_date":"2017-10-25T00:00:00+00:00",
+   "sender":{
+      "login":"%s",
+      "id":3877742,
+      "avatar_url":"https://avatars2.githubusercontent.com/u/3877742?v=4",
+      "gravatar_id":"",
+      "url":"https://api.github.com/users/username",
+      "html_url":"https://github.com/username",
+      "followers_url":"https://api.github.com/users/username/followers",
+      "following_url":"https://api.github.com/users/username/following{/other_user}",
+      "gists_url":"https://api.github.com/users/username/gists{/gist_id}",
+      "starred_url":"https://api.github.com/users/username/starred{/owner}{/repo}",
+      "subscriptions_url":"https://api.github.com/users/username/subscriptions",
+      "organizations_url":"https://api.github.com/users/username/orgs",
+      "repos_url":"https://api.github.com/users/username/repos",
+      "events_url":"https://api.github.com/users/username/events{/privacy}",
+      "received_events_url":"https://api.github.com/users/username/received_events",
+      "type":"User",
+      "site_admin":true,
+      "email":"username@email.com"
+   },
+   "marketplace_purchase":{
+      "account":{
+         "type":"Organization",
+         "id":18404719,
+         "login":"%s",
+         "organization_billing_email":"username@email.com"
+      },
+      "billing_cycle":"monthly",
+      "unit_count":1,
+      "on_free_trial":false,
+      "free_trial_ends_on":null,
+      "next_billing_date":"2017-11-05T00:00:00+00:00",
+      "plan":{
+         "id":435,
+         "name":"Public Tenant",
+         "description":"Basic Plan",
+         "monthly_price_in_cents":3200,
+         "yearly_price_in_cents":32000,
+         "price_model":"flat",
+         "has_free_trial":true,
+         "unit_name":"seat",
+         "bullets":[
+            "Is Basic",
+            "Because Basic "
+         ]
+      }
+   }
+}
+""".strip() % (self.tenant.owner.username, self.tenant.owner.username)
+        signature = utils.calculate_signature(settings.KIWI_GITHUB_MARKETPLACE_SECRET,
+                                              json.dumps(json.loads(payload)).encode())
+
+        # first simulate marketplace_purchase hook
+        response = self.client.post(self.purchase_hook_url,
+                                    json.loads(payload),
+                                    content_type='application/json',
+                                    HTTP_X_HUB_SIGNATURE=signature)
+        self.assertContains(response, 'ok')
+
+        # now login as the tenant owner
+        self.client.login(username=self.tenant.owner.username,  # nosec:B106
+                          password='password')
+
+        # and visit again the create tenant page
+        response = self.client.get(self.create_tenant_url)
+
+        fake_request = RequestFactory().get(self.create_tenant_url)
+        fake_request.user = self.tenant.owner
+        expected_url = tcms_tenants_utils.tenant_url(fake_request, self.tenant.schema_name)
+
+        # redirects to / on own tenant
+        self.assertRedirects(response, expected_url, fetch_redirect_response=False)
+
+    def test_visit_superuser(self):
+        """
+            Superuser can always create new tenant
+        """
+        payload = """
+{
+   "action":"purchased",
+   "effective_date":"2017-10-25T00:00:00+00:00",
+   "sender":{
+      "login":"%s",
+      "id":3877742,
+      "avatar_url":"https://avatars2.githubusercontent.com/u/3877742?v=4",
+      "gravatar_id":"",
+      "url":"https://api.github.com/users/username",
+      "html_url":"https://github.com/username",
+      "followers_url":"https://api.github.com/users/username/followers",
+      "following_url":"https://api.github.com/users/username/following{/other_user}",
+      "gists_url":"https://api.github.com/users/username/gists{/gist_id}",
+      "starred_url":"https://api.github.com/users/username/starred{/owner}{/repo}",
+      "subscriptions_url":"https://api.github.com/users/username/subscriptions",
+      "organizations_url":"https://api.github.com/users/username/orgs",
+      "repos_url":"https://api.github.com/users/username/repos",
+      "events_url":"https://api.github.com/users/username/events{/privacy}",
+      "received_events_url":"https://api.github.com/users/username/received_events",
+      "type":"User",
+      "site_admin":true,
+      "email":"username@email.com"
+   },
+   "marketplace_purchase":{
+      "account":{
+         "type":"Organization",
+         "id":18404719,
+         "login":"%s",
+         "organization_billing_email":"username@email.com"
+      },
+      "billing_cycle":"monthly",
+      "unit_count":1,
+      "on_free_trial":false,
+      "free_trial_ends_on":null,
+      "next_billing_date":"2017-11-05T00:00:00+00:00",
+      "plan":{
+         "id":435,
+         "name":"Public Tenant",
+         "description":"Basic Plan",
+         "monthly_price_in_cents":3200,
+         "yearly_price_in_cents":32000,
+         "price_model":"flat",
+         "has_free_trial":true,
+         "unit_name":"seat",
+         "bullets":[
+            "Is Basic",
+            "Because Basic "
+         ]
+      }
+   }
+}
+""".strip() % (self.tenant.owner.username, self.tenant.owner.username)
+        signature = utils.calculate_signature(settings.KIWI_GITHUB_MARKETPLACE_SECRET,
+                                              json.dumps(json.loads(payload)).encode())
+
+        # first simulate marketplace_purchase hook
+        response = self.client.post(self.purchase_hook_url,
+                                    json.loads(payload),
+                                    content_type='application/json',
+                                    HTTP_X_HUB_SIGNATURE=signature)
+        self.assertContains(response, 'ok')
+
+        # make tenant owner superuser
+        self.tenant.owner.is_superuser = True
+        self.tenant.owner.save()
+
+        # now login as the tenant owner
+        self.client.login(username=self.tenant.owner.username,  # nosec:B106
+                          password='password')
+
+        # and visit again the create tenant page
+        response = self.client.get(self.create_tenant_url)
+
+        # shows Create Tenant page
+        self.assertContains(response, 'New tenant')
+
+    def test_visit_after_purchase(self):
+        """
+            If user visits the Create Tenant page
+            after they've purchased a subscription plan
+            then they should be able to see the create tenant form
+            with extra fields populated.
+        """
+        payload = """
+{
+   "action":"purchased",
+   "effective_date":"2017-10-25T00:00:00+00:00",
+   "sender":{
+      "login":"%s",
+      "id":3877742,
+      "avatar_url":"https://avatars2.githubusercontent.com/u/3877742?v=4",
+      "gravatar_id":"",
+      "url":"https://api.github.com/users/username",
+      "html_url":"https://github.com/username",
+      "followers_url":"https://api.github.com/users/username/followers",
+      "following_url":"https://api.github.com/users/username/following{/other_user}",
+      "gists_url":"https://api.github.com/users/username/gists{/gist_id}",
+      "starred_url":"https://api.github.com/users/username/starred{/owner}{/repo}",
+      "subscriptions_url":"https://api.github.com/users/username/subscriptions",
+      "organizations_url":"https://api.github.com/users/username/orgs",
+      "repos_url":"https://api.github.com/users/username/repos",
+      "events_url":"https://api.github.com/users/username/events{/privacy}",
+      "received_events_url":"https://api.github.com/users/username/received_events",
+      "type":"User",
+      "site_admin":true,
+      "email":"username@email.com"
+   },
+   "marketplace_purchase":{
+      "account":{
+         "type":"Organization",
+         "id":18404719,
+         "login":"%s",
+         "organization_billing_email":"username@email.com"
+      },
+      "billing_cycle":"monthly",
+      "unit_count":1,
+      "on_free_trial":false,
+      "free_trial_ends_on":null,
+      "next_billing_date":"2017-11-05T00:00:00+00:00",
+      "plan":{
+         "id":435,
+         "name":"Public Tenant",
+         "description":"Basic Plan",
+         "monthly_price_in_cents":3200,
+         "yearly_price_in_cents":32000,
+         "price_model":"flat",
+         "has_free_trial":true,
+         "unit_name":"seat",
+         "bullets":[
+            "Is Basic",
+            "Because Basic "
+         ]
+      }
+   }
+}
+""".strip() % (self.tester.username, self.tester.username)
+        payload = json.loads(payload)
+        signature = utils.calculate_signature(settings.KIWI_GITHUB_MARKETPLACE_SECRET,
+                                              json.dumps(payload).encode())
+
+        # first simulate marketplace_purchase hook
+        response = self.client.post(self.purchase_hook_url,
+                                    payload,
+                                    content_type='application/json',
+                                    HTTP_X_HUB_SIGNATURE=signature)
+        self.assertContains(response, 'ok')
+
+        # visit the create tenant page
+        response = self.client.get(self.create_tenant_url)
+        expected_paid_until = utils.calculate_paid_until(payload['marketplace_purchase'])
+        expected_paid_until = expected_paid_until.isoformat().replace('T', ' ')
+
+        self.assertContains(response, 'New tenant')
+        self.assertContains(response, 'Private Tenant Warning')
+        self.assertContains(response, 'action="%s"' % self.create_tenant_url)
+        self.assertContains(response, 'Paid until')
+        self.assertContains(
+            response,
+            '<input type="hidden" name="paid_until" value="%s" id="id_paid_until">' %
+            expected_paid_until,
+            html=True)
+        self.assertContains(
+            response,
+            '<input type="hidden" name="on_trial" value="False" id="id_on_trial">',
+            html=True)
+        self.assertContains(response, 'Owner')
+        self.assertContains(response, "<label>%s</label>" % self.tester.username)
