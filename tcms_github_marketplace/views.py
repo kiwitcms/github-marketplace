b8e48c18a8e7e96b304286f15b0f94bce89235d9
Mr. Senko <atodorov@mrsenko.com>
2020-01-11 15:12:40 +0200

    Replace ugettext_lazy with gettext_lazy for Django 3.0

--- tcms_github_marketplace/views.py
+++ tcms_github_marketplace/views.py
@@ -10,7 +10,7 @@ from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic.base import View, TemplateView
 from django.utils.decorators import method_decorator
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.decorators import login_required
 
 from tcms.utils import github
