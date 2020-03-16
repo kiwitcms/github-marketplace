deb415257f3b99ce089b58b9b8a6a9a1a4a1a895
Mr. Senko <atodorov@mrsenko.com>
2020-03-16 22:19:17 +0200

    Update settings during testing & development
    - Remove tenant settings b/c plugin loading code will handle this
    - Make use of the settings file shipped with the package
    - Remove test_project/urls.py b/c not needed anymore

--- test_project/settings.py
+++ test_project/settings.py
@@ -1,84 +1,65 @@
 # Copyright (c) 2019 Alexander Todorov <atodorov@MrSenko.com>
 
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
+# pylint: disable=invalid-name, protected-access, wrong-import-position
 
 import os
+import sys
+import pkg_resources
+
+# pretend this is a plugin during testing & development
+# IT NEEDS TO BE BEFORE the wildcard import below !!!
+# .egg-info/ directory will mess up with this
+dist = pkg_resources.Distribution(__file__)
+entry_point = pkg_resources.EntryPoint.parse('github/marketplace = tcms_github_marketplace',
+                                             dist=dist)
+dist._ep_map = {'kiwitcms.plugins': {'kiwitcms_tenants_devel': entry_point}}
+pkg_resources.working_set.add(dist)
+
 from tcms.settings.product import *
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# check for a clean devel environment
+if os.path.exists(os.path.join(BASE_DIR, "kiwitcms_github_marketplace.egg-info")):
+    print("ERORR: .egg-info/ directories mess up plugin loading code in devel mode")
+    sys.exit(1)
+
+
+# import the settings which automatically get distributed with this package
+marketplace_settings = os.path.join(
+    BASE_DIR, 'tcms_settings_dir', 'marketplace.py')
+
+# Kiwi TCMS loads extra settings in the same way using exec()
+exec(  # pylint: disable=exec-used
+    open(marketplace_settings, "rb").read(),
+    globals()
+)
 
 # these are enabled only for testing purposes
 DEBUG = True
 TEMPLATE_DEBUG = True
-SECRET_KEY = '7d09f358-6609-11e9-8140-34363b8604e2'
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 LOCALE_PATHS = [os.path.join(BASE_DIR, 'tcms_github_marketplace', 'locale')]
 
+DATABASES['default'].update({
+    'NAME': 'test_project',
+    'USER': 'kiwi',
+    'PASSWORD': 'kiwi',
+    'HOST': 'localhost',
+    'OPTIONS': {},
+})
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django_tenants.postgresql_backend',
-        'NAME': 'test_project',
-        'USER': 'kiwi',
-        'PASSWORD': 'kiwi',
-        'HOST': 'localhost',
-        'OPTIONS': {},
-    }
-}
 
-DATABASE_ROUTERS = [
-    'django_tenants.routers.TenantSyncRouter',
-]
-
-MIDDLEWARE.insert(0, 'django_tenants.middleware.main.TenantMainMiddleware')
-
-TENANT_MODEL = "tcms_tenants.Tenant"
-TENANT_DOMAIN_MODEL = "tcms_tenants.Domain"
-
-INSTALLED_APPS.insert(0, 'django_tenants')
-INSTALLED_APPS.insert(1, 'tcms_tenants')
 INSTALLED_APPS.extend([
     'social_django',
-    'tcms_github_marketplace',
 ])
 
 PUBLIC_VIEWS.extend([
     'social_django.views.auth',
     'social_django.views.complete',
     'social_django.views.disconnect',
-    'tcms_github_marketplace.views.PurchaseHook'
 ])
 
-
-TENANT_APPS = [
-    'django.contrib.sites',
-
-    'attachments',
-    'django_comments',
-    'modernrpc',
-    'simple_history',
-
-    'tcms.core.contrib.comments.apps.AppConfig',
-    'tcms.core.contrib.linkreference',
-    'tcms.management',
-    'tcms.testcases.apps.AppConfig',
-    'tcms.testplans.apps.AppConfig',
-    'tcms.testruns.apps.AppConfig',
-]
-
-# everybody can access the main instance
-SHARED_APPS = INSTALLED_APPS
-
-# Allows serving non-public tenants on a sub-domain
-# WARNING: doesn't work well when you have a non-standard port-number
-KIWI_TENANTS_DOMAIN = 'tenants.localdomain'
-
-# share login session between tenants
-SESSION_COOKIE_DOMAIN = ".%s" % KIWI_TENANTS_DOMAIN
-
-# attachments storage
-DEFAULT_FILE_STORAGE = "tcms_tenants.storage.TenantFileSystemStorage"
-MULTITENANT_RELATIVE_MEDIA_ROOT = "tenants/%s"
-
-ROOT_URLCONF = 'test_project.urls'
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 SOCIAL_AUTH_GITHUB_KEY = 'oauth_client_id'
