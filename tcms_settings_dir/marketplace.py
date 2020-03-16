450aa15eed5e382c8ed6ef26ccfe97ad72b0fa7b
Mr. Senko <atodorov@mrsenko.com>
2020-03-16 22:24:35 +0200

    flake8 & pylint

--- tcms_settings_dir/marketplace.py
+++ tcms_settings_dir/marketplace.py
@@ -3,8 +3,8 @@
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
 # pylint: disable=undefined-variable
 
-if 'tcms_github_marketplace.views.PurchaseHook' not in PUBLIC_VIEWS:  # noqa: F821
-    PUBLIC_VIEWS.append('tcms_github_marketplace.views.PurchaseHook') # noqa: F821
+if 'tcms_github_marketplace.views.PurchaseHook' not in PUBLIC_VIEWS:   # noqa: F821
+    PUBLIC_VIEWS.append('tcms_github_marketplace.views.PurchaseHook')  # noqa: F821
 
-if 'tcms_github_marketplace.views.FastSpringHook' not in PUBLIC_VIEWS:  # noqa: F821
-    PUBLIC_VIEWS.append('tcms_github_marketplace.views.FastSpringHook') # noqa: F821
+if 'tcms_github_marketplace.views.FastSpringHook' not in PUBLIC_VIEWS:   # noqa: F821
+    PUBLIC_VIEWS.append('tcms_github_marketplace.views.FastSpringHook')  # noqa: F821
