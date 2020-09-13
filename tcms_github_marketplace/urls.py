3b2bb5a4e485032ff04eb0d1bfb439657b98160c
Mr. Senko <atodorov@mrsenko.com>
2020-09-13 15:39:28 +0300

    Use re_path() instead of deprecated url() shortcut

--- tcms_github_marketplace/urls.py
+++ tcms_github_marketplace/urls.py
@@ -1,15 +1,17 @@
-# Copyright (c) 2019 Alexander Todorov <atodorov@MrSenko.com>
+# Copyright (c) 2019-2020 Alexander Todorov <atodorov@MrSenko.com>
 
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
 
-from django.conf.urls import url
+from django.urls import re_path
 from tcms_github_marketplace import views
 
 
 urlpatterns = [
-    url(r'^purchase_hook/$', views.PurchaseHook.as_view(), name='github_marketplace_purchase_hook'),
-    url(r'^install/$', views.Install.as_view(), name='github_marketplace_install'),
-    url(r'^create/tenant/$', views.CreateTenant.as_view(), name='github_marketplace_create_tenant'),
-    url(r'^plans/$', views.ViewSubscriptionPlan.as_view(), name='github_marketplace_plans'),
-    url(r'^fastspring/$', views.FastSpringHook.as_view(), name='fastspring'),
+    re_path(r'^purchase_hook/$', views.PurchaseHook.as_view(),
+            name='github_marketplace_purchase_hook'),
+    re_path(r'^install/$', views.Install.as_view(), name='github_marketplace_install'),
+    re_path(r'^create/tenant/$', views.CreateTenant.as_view(),
+            name='github_marketplace_create_tenant'),
+    re_path(r'^plans/$', views.ViewSubscriptionPlan.as_view(), name='github_marketplace_plans'),
+    re_path(r'^fastspring/$', views.FastSpringHook.as_view(), name='fastspring'),
 ]
