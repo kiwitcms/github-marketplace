de3ebb576082ecbf2574f21e9aeda1e3ce71c05b
Alexander Todorov <atodorov@otb.bg>
2023-04-08 17:39:20 +0300

    Add more tests for FastSpringHook
    - test that invalid request signatures return 403
    - test for both subscription.charge.completed event

--- tcms_github_marketplace/tests/test_fastspring_hook.py
+++ tcms_github_marketplace/tests/test_fastspring_hook.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022 Alexander Todorov <atodorov@MrSenko.com>
+# Copyright (c) 2022-2023 Alexander Todorov <atodorov@MrSenko.com>
 
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
 # pylint: disable=too-many-ancestors, too-many-lines
@@ -6,12 +6,14 @@ import json
 import hmac
 import hashlib
 
-from unittest.mock import call, patch
 from base64 import b64encode
+from datetime import datetime
+from unittest.mock import call, patch
 
 from django.urls import reverse
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.http import HttpResponseForbidden
 
 from social_django.models import UserSocialAuth
 
@@ -286,7 +288,7 @@ class FastSpringHookTestCase(tcms_tenants.tests.LoggedInTestCase):
             get_user_model().objects.filter(username=self.tester.username).exists()
         )
 
-    def test_subscription_activated(self):
+    def test_initial_subscription(self):
         payload = """
 {
     "events": [
@@ -569,6 +571,311 @@ class FastSpringHookTestCase(tcms_tenants.tests.LoggedInTestCase):
                 should_have_tenant=True,
             ).exists()
         )
+        # this is an initial subscription so Tenant hasn't been created yet The used needs
+        # to do this by visiting the Create Tenant page
+        self.assertFalse(
+            tcms_tenants.models.Tenant.objects.filter(owner=self.tester).exists()
+        )
+
+    def test_recurring_billing(self):
+        event_timestamp = 1643836213792
+
+        # simulate a tenant which was already created
+        self.tenant.owner = self.tester
+        # NB: FastSpring sends the effective_date as an integer timestamp
+        self.tenant.paid_until = datetime.fromtimestamp(event_timestamp / 1000)
+        self.tenant.save()
+        original_paid_until = self.tenant.paid_until
+
+        payload = """
+{
+    "events": [
+        {
+          "id": "9cMkAyvIT6aha_KwS0M6aw",
+          "data": {
+            "id": "_fmtng11QUyLno5-5aVB-w",
+            "end": null,
+            "sku": "x-tenant+version",
+            "live": true,
+            "next": 1646179200000,
+            "adhoc": false,
+            "begin": 1643760000000,
+            "price": 56,
+            "quote": null,
+            "state": "active",
+            "active": true,
+            "account": {
+              "id": "vvvv",
+              "url": "https://mrsenko.onfastspring.com/account",
+              "lookup": {
+                "global": "SSSSS"
+              },
+              "account": "vvvv",
+              "address": {
+                "city": null,
+                "region": null,
+                "company": "BG",
+                "postal code": null,
+                "region custom": null,
+                "address line 1": null,
+                "address line 2": null
+              },
+              "contact": {
+                "first": "%s",
+                "last": "%s",
+                "email": "%s",
+                "phone": "000",
+                "company": "Kiwi TCMS",
+                "subscribed": true
+              },
+              "country": "BG",
+              "language": "en"
+            },
+            "changed": 1643836213623,
+            "display": "Kiwi TCMS Private Tenant",
+            "periods": null,
+            "product": {
+              "sku": "version",
+              "image": "https://d8y8nchqlnmka.cloudfront.net/XAeq54smTQ8/gahPnpQiREI/square-with-name.png",
+              "format": "digital",
+              "parent": null,
+              "display": {
+                "en": "Kiwi TCMS Private Tenant"
+              },
+              "pricing": {
+                "price": {
+                  "USD": 50
+                },
+                "renew": "auto",
+                "interval": "month",
+                "cancellation": {
+                  "interval": "week",
+                  "intervalLength": 1
+                },
+                "intervalCount": null,
+                "intervalLength": 1,
+                "quantityDefault": 1,
+                "quantityBehavior": "allow",
+                "dateLimitsEnabled": false,
+                "overdueNotification": {
+                  "amount": 4,
+                  "enabled": true,
+                  "interval": "week",
+                  "intervalLength": 1
+                },
+                "reminderNotification": {
+                  "enabled": true,
+                  "interval": "week",
+                  "intervalLength": 1
+                }
+              },
+              "product": "kiwitcms-private-tenant",
+              "taxcode": "SW054000",
+              "description": {
+                "summary": {
+                  "en": "Unlimited users."
+                }
+              },
+              "fulfillments": {
+                "kiwitcms-private-tenant_email_0": {
+                  "name": "Email (#{orderItem.display} Delivery ...)",
+                  "fulfillment": "kiwitcms-private-tenant_email_0",
+                  "applicability": "NON_REBILL_ONLY"
+                }
+              },
+              "taxcodeDescription": "Cloud Services - SaaS - Service Agreement",
+              "productAppReference": "7yDTCEDoSt6S44eWtgVc_g"
+            },
+            "currency": "EUR",
+            "discount": 0,
+            "endValue": null,
+            "quantity": 1,
+            "sequence": 1,
+            "subtotal": 46.67,
+            "autoRenew": true,
+            "nextValue": 1646179200000,
+            "beginValue": 1643760000000,
+            "endDisplay": null,
+            "nextDisplay": "3/2/22",
+            "beginDisplay": "2/2/22",
+            "canceledDate": null,
+            "changedValue": 1643836213623,
+            "endInSeconds": null,
+            "fulfillments": {},
+            "instructions": [
+              {
+                "type": "regular",
+                "price": 56,
+                "total": 56,
+                "product": "kiwitcms-private-tenant",
+                "unitPrice": 56,
+                "priceTotal": 56,
+                "intervalUnit": "month",
+                "priceDisplay": "€56.00",
+                "totalDisplay": "€56.00",
+                "unitDiscount": 0,
+                "discountTotal": 0,
+                "periodEndDate": null,
+                "intervalLength": 1,
+                "discountPercent": 0,
+                "periodStartDate": null,
+                "unitPriceDisplay": "€56.00",
+                "priceTotalDisplay": "€56.00",
+                "periodEndDateValue": null,
+                "unitDiscountDisplay": "€0.00",
+                "discountPercentValue": 0,
+                "discountTotalDisplay": "€0.00",
+                "periodEndDateDisplay": null,
+                "periodStartDateValue": null,
+                "priceInPayoutCurrency": 60.81,
+                "totalInPayoutCurrency": 60.81,
+                "periodEndDateInSeconds": null,
+                "periodStartDateDisplay": null,
+                "periodStartDateInSeconds": null,
+                "unitPriceInPayoutCurrency": 60.81,
+                "priceTotalInPayoutCurrency": 60.81,
+                "priceInPayoutCurrencyDisplay": "$60.81",
+                "totalInPayoutCurrencyDisplay": "$60.81",
+                "unitDiscountInPayoutCurrency": 0,
+                "discountTotalInPayoutCurrency": 0,
+                "unitPriceInPayoutCurrencyDisplay": "$60.81",
+                "priceTotalInPayoutCurrencyDisplay": "$60.81",
+                "unitDiscountInPayoutCurrencyDisplay": "$0.00",
+                "discountTotalInPayoutCurrencyDisplay": "$0.00"
+              }
+            ],
+            "intervalUnit": "month",
+            "priceDisplay": "€56.00",
+            "subscription": "_fmtng11QUyLno5-5aVB-w",
+            "nextInSeconds": 1646179200,
+            "beginInSeconds": 1643760000,
+            "changedDisplay": "2/2/22",
+            "intervalLength": 1,
+            "nextChargeDate": 1646179200000,
+            "paymentOverdue": {
+              "sent": 0,
+              "total": 4,
+              "intervalUnit": "week",
+              "intervalLength": 1
+            },
+            "discountDisplay": "€0.00",
+            "nextChargeTotal": 46.67,
+            "paymentReminder": {
+              "intervalUnit": "week",
+              "intervalLength": 1
+            },
+            "subtotalDisplay": "€46.67",
+            "changedInSeconds": 1643836213,
+            "deactivationDate": null,
+            "nextChargePreTax": 46.67,
+            "remainingPeriods": null,
+            "taxExemptionData": "BG200",
+            "canceledDateValue": null,
+            "nextChargeCurrency": "EUR",
+            "canceledDateDisplay": null,
+            "cancellationSetting": {
+              "cancellation": "AFTER_LAST_NOTIFICATION",
+              "intervalUnit": "week",
+              "intervalLength": 1
+            },
+            "nextChargeDateValue": 1646179200000,
+            "nextNotificationDate": 1645574400000,
+            "nextNotificationType": "PAYMENT_REMINDER",
+            "canceledDateInSeconds": null,
+            "deactivationDateValue": null,
+            "nextChargeDateDisplay": "3/2/22",
+            "priceInPayoutCurrency": 60.81,
+            "nextChargeTotalDisplay": "€46.67",
+            "deactivationDateDisplay": null,
+            "nextChargeDateInSeconds": 1646179200,
+            "nextChargePreTaxDisplay": "€46.67",
+            "discountInPayoutCurrency": 0,
+            "subtotalInPayoutCurrency": 50.68,
+            "deactivationDateInSeconds": null,
+            "nextNotificationDateValue": 1645574400000,
+            "nextNotificationDateDisplay": "2/23/22",
+            "priceInPayoutCurrencyDisplay": "$60.81",
+            "nextNotificationDateInSeconds": 1645574400,
+            "discountInPayoutCurrencyDisplay": "$0.00",
+            "nextChargeTotalInPayoutCurrency": 50.68,
+            "subtotalInPayoutCurrencyDisplay": "$50.68",
+            "nextChargePreTaxInPayoutCurrency": 50.68,
+            "nextChargeTotalInPayoutCurrencyDisplay": "$50.68",
+            "nextChargePreTaxInPayoutCurrencyDisplay": "$50.68"
+          },
+          "live": true,
+          "type": "subscription.charge.completed",
+          "created": %d,
+          "processed": false,
+          "marketplace_purchase": {
+            "plan": {
+              "monthly_price_in_cents": 5068
+            },
+            "account": {
+              "type": "User"
+            },
+            "billing_cycle": "monthly"
+          }
+        }
+    ]
+}
+""".strip() % (
+            self.tester.first_name,
+            self.tester.last_name,
+            self.tester.email,
+            event_timestamp,
+        )
+
+        signature = self.calculate_signature(payload)
+
+        initial_purchase_count = Purchase.objects.count()
+        self.assertFalse(
+            Purchase.objects.filter(
+                vendor="fastspring", action="purchased", sender=self.tester.email
+            ).exists()
+        )
+
+        # tmp_account calculates the actual robot name for mocking - currently not in use
+        with docker.QuayIOAccount(self.tester.email) as tmp_account:
+            with patch.object(
+                docker.QuayIOAccount,
+                "create",
+                return_value={"name": tmp_account.name, "token": "secret"},
+            ) as quay_io_create, patch.object(
+                docker.QuayIOAccount,
+                "allow_read_access",
+                return_value="success",
+            ) as quay_io_allow_read_access, patch.object(
+                mailchimp,
+                "subscribe",
+                return_value="success",
+            ) as mailchimp_subscribe:
+                response = self.client.post(
+                    self.purchase_hook_url,
+                    json.loads(payload),
+                    content_type="application/json",
+                    HTTP_X_FS_SIGNATURE=signature,
+                )
+                self.assertContains(response, "ok")
+
+                # these happen only for initial purchases
+                quay_io_create.assert_not_called()
+                quay_io_allow_read_access.assert_not_called()
+                mailchimp_subscribe.assert_not_called()
+
+        self.assertEqual(initial_purchase_count + 1, Purchase.objects.count())
+        self.assertTrue(
+            Purchase.objects.filter(
+                vendor="fastspring",
+                action="purchased",
+                sender=self.tester.email,
+                should_have_tenant=True,
+            ).exists()
+        )
+        # this is an initial subscription so Tenant hasn't been created yet The used needs
+        # to do this by visiting the Create Tenant page
+        self.tenant.refresh_from_db()
+        self.assertGreater(self.tenant.paid_until, original_paid_until)
 
     def test_subscription_activated_fallback_sku_private_tenant(self):
         payload = """
@@ -1131,3 +1438,18 @@ class FastSpringHookTestCase(tcms_tenants.tests.LoggedInTestCase):
                 should_have_tenant=True,
             ).exists()
         )
+
+    def test_request_signature_is_not_valid(self):
+        payload = """
+{
+    "events": [
+    ]
+}
+"""
+        response = self.client.post(
+            self.purchase_hook_url,
+            json.loads(payload),
+            content_type="application/json",
+            HTTP_X_FS_SIGNATURE="this-is-wrong",
+        )
+        self.assertIsInstance(response, HttpResponseForbidden)
