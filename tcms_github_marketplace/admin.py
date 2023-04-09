5ef1e3457ed0b8ffc928283db12051724ff2a4b6
Alexander Todorov <atodorov@otb.bg>
2023-04-09 23:44:08 +0300

    Add Admin interface so we can 'Approve' manual purchases
    - an administrator will record a purchase after it has been charged to
      the customer. Usually via invoice & direct wire transfer
    - the purchase will be recorded as usual in the database
    - customer will be sent a fulfillment email with instructions how to
      provision SaaS instances and/or get access to digital downloads

--- tcms_github_marketplace/admin.py
+++ tcms_github_marketplace/admin.py
@@ -1,12 +1,17 @@
-# Copyright (c) 2019 Alexander Todorov <atodorov@MrSenko.com>
+# Copyright (c) 2019-2023 Alexander Todorov <atodorov@MrSenko.com>
 
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
 
+import json
+
+from django import forms
+from django import http
 from django.urls import reverse
+from django.utils import timezone
 from django.contrib import admin
 from django.http import HttpResponseForbidden, HttpResponseRedirect
 
-from tcms_github_marketplace.models import Purchase
+from tcms_github_marketplace.models import ManualPurchase, Purchase
 
 
 class PurchaseAdmin(admin.ModelAdmin):
@@ -66,4 +71,114 @@ class PurchaseAdmin(admin.ModelAdmin):
         )
 
 
+# pylint: disable=form-field-label-used, form-field-help-text-used
+class ManualPurchaseForm(forms.ModelForm):
+    invoice = forms.CharField(
+        required=True,
+        label="Invoice No.",
+        help_text="NUM-YYYY-MM-DD. Will be used as the Subscription ID",
+    )
+    price = forms.IntegerField(
+        required=True, label="Price in USD", help_text="Per billing cycle!"
+    )
+    billing_cycle = forms.ChoiceField(
+        required=True,
+        choices=(
+            ("", "-----"),
+            ("yearly", "Yearly"),
+            ("monthly", "Monthly"),
+        ),
+    )
+    customer_name = forms.CharField(required=True)
+    address = forms.CharField(required=True)
+    billing_email = forms.EmailField(
+        required=True,
+        help_text="will receive product renewal information",
+    )
+    technical_email = forms.EmailField(
+        required=True,
+        help_text="will receive product access and will be subscribed to newsletter",
+    )
+    sku = forms.CharField(
+        required=True,
+        max_length=256,
+        label="SKU",
+        help_text="e.g: x-tenant+version+enterprise",
+    )
+
+    class Meta:
+        model = ManualPurchase
+        fields = "__all__"
+
+
+class ManualPurchaseAdmin(admin.ModelAdmin):
+    form = ManualPurchaseForm
+
+    @staticmethod
+    def calculate_price_in_cents(form):
+        billing_cycle = form.cleaned_data["billing_cycle"]
+        monthly_price = 0
+        yearly_price = 0
+
+        if billing_cycle == "monthly":
+            monthly_price = form.cleaned_data["price"] * 100
+            yearly_price = monthly_price * 12
+        elif billing_cycle == "yearly":
+            yearly_price = form.cleaned_data["price"] * 100
+            monthly_price = int(yearly_price / 12)
+        else:
+            raise RuntimeError(f"Unrecognized billong cycle '{billing_cycle}'")
+
+        return (monthly_price, yearly_price)
+
+    def save_model(self, request, obj, form, change):
+        monthly_price, yearly_price = self.calculate_price_in_cents(form)
+
+        # WARNING that payload needs to be a list of events!
+        request.purchase_payload = [
+            {
+                "action": "purchased",
+                "effective_date": timezone.now().isoformat(),
+                "data": form.cleaned_data,
+                "marketplace_purchase": {
+                    "account": {
+                        "type": "User",
+                    },
+                    "billing_cycle": form.cleaned_data["billing_cycle"],
+                    "plan": {
+                        "monthly_price_in_cents": monthly_price,
+                        "yearly_price_in_cents": yearly_price,
+                    },
+                },
+            }
+        ]
+
+    def response_add(
+        self, request, obj, post_url_continue=None
+    ):  # pylint: disable=protected-access
+        """
+        Gets called after self.save_model() and determines the response
+        after a new object has been added. Must return an HttpResponse instance!
+
+        Builds an internal POST request containing JSON data about the subscription
+        and routes it internally to the corresponding view!
+        """
+        post_body = json.dumps(request.purchase_payload).encode("utf-8")
+        request.META["REQUEST_METHOD"] = "POST"
+        request.META["CONTENT_LENGTH"] = len(post_body)
+        request.META["CONTENT_TYPE"] = "application/json"
+
+        new_request = request.__class__(request.META)
+        new_request.purchase_payload = request.purchase_payload
+        new_request._body = post_body
+        new_request._post = http.QueryDict(post_body, encoding=new_request._encoding)
+
+        from .views import (  # pylint: disable=import-outside-toplevel
+            ProcessManualPurchase,
+        )
+
+        return ProcessManualPurchase.as_view()(new_request)
+
+
+admin.site.register(ManualPurchase, ManualPurchaseAdmin)
 admin.site.register(Purchase, PurchaseAdmin)
