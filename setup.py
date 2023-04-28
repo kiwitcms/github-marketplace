f4b0d5c992e0aeae7aea7a03bfffc8855bdbac2e
Alexander Todorov <atodorov@otb.bg>
2023-04-28 13:56:41 +0300

    Changelog for v2.3.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def get_install_requires(path):
 
 setup(
     name="kiwitcms-github-marketplace",
-    version="2.3.1",
+    version="2.3.2",
     description="GitHub Marketplace integration for Kiwi TCMS",
     long_description=get_long_description(),
     author="Kiwi TCMS",
