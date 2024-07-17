960fbd36a8f3be0dc68833413e33345248ed705a
Alexander Todorov <atodorov@otb.bg>
2024-07-17 22:34:44 +0300

    Prefix FastSpring subscription_id with fs-

---
+++ tcms_github_marketplace/migrations/0010_prefix_fastspring_subscription_id.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2024 Alexander Todorov <atodorov@otb.bg>
+#
+# Licensed under GNU Affero General Public License v3 or later (AGPLv3+)
+# https://www.gnu.org/licenses/agpl-3.0.html
+
+from django.db import migrations
+
+
+def forwards(apps, schema_editor):  # pylint: disable=unused-argument
+    purchase_model = apps.get_model("tcms_github_marketplace", "Purchase")
+    for purchase in purchase_model.objects.filter(vendor="fastspring"):
+        try:
+            purchase.subscription = f"fs-{purchase.subscription}"
+            purchase.save()
+        except:  # noqa, pylint: disable=bare-except
+            pass
+
+
+def backwards(apps, schema_editor):  # pylint: disable=unused-argument
+    purchase_model = apps.get_model("tcms_github_marketplace", "Purchase")
+    for purchase in purchase_model.objects.filter(vendor="fastspring"):
+        try:
+            purchase.subscription = purchase.subscription.lstrip("fs-")
+            purchase.save()
+        except:  # noqa, pylint: disable=bare-except
+            pass
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("tcms_github_marketplace", "0009_github_subscription_id"),
+    ]
+
+    operations = [
+        migrations.RunPython(forwards, backwards),
+    ]
