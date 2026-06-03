1b397327953bc3e2a4bd6ba44de95a0a0d012fb4
Alexander Todorov <atodorov@otb.bg>
2026-06-03 14:57:07 +0300

    Build & test with Python 3.12

--- setup.py
+++ setup.py
@@ -47,8 +47,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
 )
